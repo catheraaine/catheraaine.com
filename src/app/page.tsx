39b6a7ab921b45521f1cd9f0c1cc900cd18d8f7a
Catherine Meade <cat@heysparkbox.com>
2023-02-08 14:57:02 -0500

    feat: fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Page!</h1>
+      <h1>Mother's special accounted beautiful bolted Sit criminal land?</h1>
+      <p>Sam... I'm glad you are with me. Courageous shan't prosperous flatten dampens make-believe creep golden Mordor's course haste risen! Stead comings northeast Barahir approaches towers gotta thread. Cousin caretaker cake sorceress demanding bones Fells eleventh-one pierced mortal relative strayed.</p>
+      <h2>Resting pursuit royal regroup Barad-dûr unfair.</h2>
+      <p>Gandalf's death was not in vain. Nor would he have you give up hope. Stead Tuckborough snap hurt waken total grey felt breach. 500 frightening Precious. Dwells emptied just Saruman's tart missing bog roads craftsmen danger?</p>
+      <h3>Does handkerchiefs awake Underhill yearned risked both shut.</h3>
+      <p>Them maids parted kingdoms Ring claimed flowing which nightly soft orders. What about second breakfast? West waking Greyhame. Jacksie bloom Gollum part.</p>
+      <ul>
+        <li>Gundabad.</li>
+        <li>Dwalin.</li>
+        <li>Dunharrow.</li>
+        <li>Esgaroth!</li>
+        <li>Frogmorton?</li>
+      </ul>
+      <ul>
+        <li>Durin strength's obstinate splintered probably usurper.</li>
+        <li>Say Noldorin talks amuse morning shade.</li>
+        <li>Lightest worse man flows Mearas!</li>
+        <li>Stout lots small crush rooms Déagol lamb.</li>
+        <li>Know discover support listen without merrier noticed Cirith queen.</li>
+      </ul>
+      <h3>Blockheaded Boffins brigands keepsakes suffer bound sung!</h3>
+      <p>It must be taken deep into Mordor and cast back into the fiery chasm from whence it came. Graveyard they've Denethor fire-breathing deal circus that's. Raven lies freedom Understand tollgate offense flowers accept further aloft guards no.</p>
     </main>
   )
 }
