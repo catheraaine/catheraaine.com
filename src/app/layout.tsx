762eccb05f75959f7614c2930254a4f3492f907b
Catherine Meade <cat@heysparkbox.com>
2023-02-08 11:15:23 -0500

    feat: sass and sass linting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
-import './globals.css'
+import '../styles/globals.scss';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
-}) {
+}): JSX.Element {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
