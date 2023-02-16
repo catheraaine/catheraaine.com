2a891cc6af30065a48eca858d21cd675bd1e37c9
Catherine Meade <cat@heysparkbox.com>
2023-02-16 14:34:54 -0500

    feat: placeholder navigation and styles

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,7 +1,9 @@
 export const Navigation = () => {
   return (
     <nav className="navigation">
-      ♥
+      <a href="#" className="navigation__link">About</a>
+      <span className="navigation__spacer" aria-hidden="true">♥</span>
+      <a href="#" className="navigation__link">Contact</a>
     </nav>
   )
 };
