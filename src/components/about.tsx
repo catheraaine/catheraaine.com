26d3ef62088d293f3acb6e4fc48a7ea84c2d0d06
Catherine Meade <cat@heysparkbox.com>
2023-02-20 12:04:33 -0500

    feat: little about page tweaks

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,8 +2,8 @@ export const About = () => {
   return (
     <section id="about" className="about">
       <div className="about__text">
-        <h2>About Me</h2>
-        <p>Catherine manages teams at Sparkbox as a Technical Director, delivering responsive, accessible web experiences with a primary focus on enterprise-level eCommerce applications and design systems. With 15+ years of development experience, she specializes in React Applications and UI Components. She's also a Certified Professional in Web Accessibility (CPWA) and graduated with a B.S. from Miami University of Ohio.</p>
+        <h2>About Catherine</h2>
+        <p>Catherine manages teams at <a href="https://sparkbox.com/">Sparkbox</a> as a Technical Director, delivering responsive, accessible web experiences with a primary focus on enterprise-level eCommerce applications and design systems. With 15+ years of development experience, she specializes in React Applications and UI Components. She's also a <a href="https://www.accessibilityassociation.org/s/certified-professional-web-accessibility">Certified Professional in Web Accessibility (CPWA)</a> and graduated with a B.S. from <a href="https://www.miamialum.org/s/916/22/Interior.aspx?sid=916&gid=1&pgid=17551">Miami University of Ohio</a>.</p>
       </div>
     </section>
   )
