f8413358ecfbbd6249e30c083e6e8a6bd61a36bf
Catherine Meade <cat@heysparkbox.com>
2024-02-21 10:35:22 -0500

    chore: remove security vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  /* config options here */
 }
 
 module.exports = nextConfig
